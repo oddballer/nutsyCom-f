b74d9033db3c1a685812d5bd2849ca986ac9e926
AJ Page <aj.page@techelevator.com>
2025-07-17 11:54:43 -0600

    remove index.css reference from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import './index.css'
 import App from './App.jsx'
 
 import original from 'react95/dist/themes/original';
